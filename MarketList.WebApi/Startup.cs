738d1b699f74c607a5eebffcd16a88128ec62050
Vinicius Rodrigues Santos <vrodrisa@everis.com>
2018-10-26 15:15:28 -0200

    add swagger in webapi

--- MarketList.WebApi/Startup.cs
+++ MarketList.WebApi/Startup.cs
@@ -10,39 +10,45 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
-namespace MarketList.WebApi
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace MarketList.WebApi {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc()
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ()
+                .SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new Info { Title = "MarketList", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseHttpsRedirection ();
+
+            app.UseSwagger ();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI (c => {
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "MarketList.WebApi");
+            });
+
+            app.UseMvc ();
         }
     }
 }
