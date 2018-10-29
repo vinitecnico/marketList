b030a32e2c6b1a33e2a4f56b5b88d01779a201d0
vinicius rodrigues <vinitecnico@gmail.com>
2018-10-29 19:53:53 -0300

    fix controller

--- MarketList.WebApi/Repository/SupermarketRepository.cs
+++ MarketList.WebApi/Repository/SupermarketRepository.cs
@@ -4,36 +4,43 @@ using MarketList.WebApi.Model;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
-namespace MarketList.WebApi.Repository {
-    public class SupermarketRepository : IRepository<Supermarket> {
+namespace MarketList.WebApi.Repository
+{
+    public class SupermarketRepository : IRepository<Supermarket>
+    {
         private readonly IContext<Supermarket> _context;
 
-        public SupermarketRepository (IContext<Supermarket> context) {
+        public SupermarketRepository(IContext<Supermarket> context)
+        {
             _context = context;
         }
 
-        public async Task Create (Supermarket item) {
-            await _context.collection.InsertOneAsync (item);
+        public async Task Create(Supermarket item)
+        {
+            await _context.collection.InsertOneAsync(item);
         }
 
-        public async Task<bool> Delete (string id) {
-            var result = await _context.collection.FindOneAndDeleteAsync (x => x.Id.ToString () == id);
+        public async Task<bool> Delete(string id)
+        {
+            var result = await _context.collection.FindOneAndDeleteAsync(x => x.Id == ObjectId.Parse(id));
             return true;
         }
 
-        public async Task<IEnumerable<Supermarket>> GetAll () {
-            return await _context.collection.Find (_ => true).ToListAsync ();
+        public async Task<IEnumerable<Supermarket>> GetAll()
+        {
+            return await _context.collection.Find(_ => true).ToListAsync();
         }
 
-        public async Task<Supermarket> GetById (string id) {
-            return await _context.collection.Find (x => x.Id == ObjectId.Parse (id)).FirstOrDefaultAsync ();
+        public async Task<Supermarket> GetById(string id)
+        {
+            return await _context.collection.Find(x => x.Id == ObjectId.Parse(id)).FirstOrDefaultAsync();
         }
 
-        public async Task Update (string id, Supermarket item) {
-            var filter2 = Builders<Supermarket>.Filter.Eq ("Id", id);
-            var update2 = Builders<Supermarket>.Update.Inc (c => c.ProductName, item.ProductName);
-            var options2 = new FindOneAndUpdateOptions<Supermarket> { IsUpsert = true, ReturnDocument = ReturnDocument.After };
-            await _context.collection.FindOneAndUpdateAsync (filter2, update2, options2);
+        public async Task Update(string id, Supermarket item)
+        {
+            var filter = Builders<Supermarket>.Filter.Eq(s => s.Id, ObjectId.Parse(id));
+            var update = Builders<Supermarket>.Update.Set(s => s.ProductName, item.ProductName);
+            await _context.collection.UpdateOneAsync(filter, update);
         }
     }
 }
